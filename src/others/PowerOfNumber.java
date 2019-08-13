639e1d0e48bfe6f8d047e95893f069b4fbc6fc1a
Parveen Dala <dparveen94@gmail.com>
2019-08-13 19:28:25 +0530

    Power of number

---
+++ src/others/PowerOfNumber.java
@@ -0,0 +1,20 @@
+package others;
+
+/******
+ * Parveen D
+ * Power of number.
+ */
+public class PowerOfNumber {
+    public static void main(String... args) {
+        System.out.println("Power of number : " + (powerOfNumber(2, 10)));
+    }
+
+    private static int powerOfNumber(int m, int n) {
+        if (n == 0)
+            return 1;
+        else if (n % 2 == 0)
+            return powerOfNumber(m * m, n / 2);
+        else
+            return m * powerOfNumber(m * m, (n - 1) / 2);
+    }
+}
