17a29d4d3fb40ad24ed12663929d4f3ca0355833
Parveen Dala <dparveen94@gmail.com>
2019-08-13 23:10:06 +0530

    Tower of hanoi

---
+++ src/others/TowerOfHanoi.java
@@ -0,0 +1,19 @@
+package others;
+
+/******
+ * Parveen D
+ * Tower of hanoi.
+ */
+public class TowerOfHanoi {
+    public static void main(String[] args) {
+        toh(3, 'A', 'B', 'C');
+    }
+
+    private static void toh(int n, char a, char b, char c) {
+        if (n > 0) {
+            toh(n - 1, a, c, b);
+            System.out.println(a + " > " + c);
+            toh(n - 1, b, a, c);
+        }
+    }
+}
